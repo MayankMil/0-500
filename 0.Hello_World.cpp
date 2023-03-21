603c0fa6f39fbce2eb537aea9a08a8606e5e0029
MASTER_MAYANK <99325312+MayankMil@users.noreply.github.com>
2023-03-21 22:54:13 +0530

    Create 0.Hello_World.cpp

---
+++ 0.Hello_World.cpp
@@ -0,0 +1,15 @@
+//Preprocessor directive used for including header files used for input / output operations.
+#include <iostream>
+
+//This is used to tell compiler which namespace we are using to avoid namespace collisions.
+using namespace std;
+
+//Starting of any C++ Program from main function.
+int main() {
+  
+  //
+  cout << "Hello_World!" << endl;
+  
+  return 0;
+  
+}
