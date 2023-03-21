424d611df13c99ace9319e66957d1b76d180cb99
Suraj Usrethe <104275415+surajusrethe@users.noreply.github.com>
2023-03-22 04:36:07 +0530

    Create 0.Hellow_World.java
    helloworld in java

---
+++ 0.Hellow_World.java
@@ -0,0 +1,21 @@
+//Hi Suraj here, let me introduce you to Java.
+
+//Simple and short keep in mind anything starting with capital letter Cosidered as Class in java. so here Main.
+
+// Public means this particular method is available to the entire package and also with the other classes.
+public class Main
+{
+  //this line is a function or method in java which is public and static means you can use method without creating object in java, Chill rest we study later.
+	public static void main(String[] args) 
+  {
+    
+    //this perticular line used to print statement in java, 
+    
+    //System Refers to class in Java, which provides methods and properties related to the system itself, such as input/output.
+    
+    //Refers to the println() method, which is a shorthand for "print line" 
+    
+    //"" <- this is used for string which can be any sentence or single letter or word in java.
+		System.out.println("Hello World");
+	}
+}
